50840735f9cd933197d5d1d514e0ab9b44de6a10
Hanselpare <126003026+Hanselpare@users.noreply.github.com>
2023-06-12 18:49:42 -0600

    Add files via upload

---
+++ SentenciaFor/src/sentenciafor/SentenciaFor.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sentenciafor;
+
+/**
+ *
+ * @author hanpa
+ */
+public class SentenciaFor {
+//primer ejemplo
+    public static void main(String[] args) {
+    int[][] matriz = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+for (int i = 0; i < matriz.length; i++) {
+  for (int j = 0; j < matriz[i].length; j++) {
+    System.out.print(matriz[i][j] + " ");
+  }
+  System.out.println();
+}
+    }
+    
+    
+    //segundo ejemplo
+
+
+
+}
---
+++ SentenciaFor/src/sentenciafor/nombres.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sentenciafor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author hanpa
+ */
+
+//segundo ejemplo
+class nombres {
+List<String> nombres = new ArrayList<>();
+public void a(){
+nombres.add("Juan");
+nombres.add("María");
+nombres.add("Pedro");
+
+for (String nombre : nombres) {
+    System.out.println(nombre);
+}
+}
+}
+
+
